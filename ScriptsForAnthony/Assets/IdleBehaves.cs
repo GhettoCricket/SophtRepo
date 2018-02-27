ac619ac0affe9164b6d3b40e8f3d33787a72433b
deggleston <10671954@my.uvu.edu>
2018-02-27 13:03:26 -0700

    Added Scriptable Object Project

---
+++ ScriptsForAnthony/Assets/IdleBehaves.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBehaves : StateMachineBehaviour {
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		
+	}
+}
